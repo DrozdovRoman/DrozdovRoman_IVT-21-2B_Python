d7df9a17a047624b3d472f3e56c8b692e5b771f7
a1ir <romandrozdov@icloud.com>
2022-11-15 21:13:38 +0500

    perform Lab1

---
+++ Lab_1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import math
+
+task1 = np.arange(1000000)
+task2 = np.random.randint(1,10,1000000)
+
+print("Задание 1:\nВычислить сумму ряда от 0 до 999999\nОтвет: ", task1.sum())
+
+print()
+
+print("Задание 2:\nСреднее арифметическое этого ряда\nОтвет: ", task1.mean())
+
+print()
+
+print("Задание 3:\nМедиана случайных 1000000 чисел\nОтвет: ", np.median(task2))
+
+print()
+
+print("Задание 4:\nПроизведение случайных 1000000 чисел\nОтвет: ", np.prod(task2, dtype=object))
+
